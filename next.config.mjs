3b793db6dceb90d596f614921fd11647e1199557
ByungJoon Lee <imjuni@gmail.com>
2024-06-23 19:14:21 +0900

    feat(static): add new configuration that static site generation

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
+import nextBundleAnalyzer from '@next/bundle-analyzer';
+import createNextIntlPlugin from 'next-intl/plugin';
+
+const withBundleAnalyzer = nextBundleAnalyzer({ enabled: process.env.ANALYZE === 'true' });
+const withNextIntlPlugin = createNextIntlPlugin();
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfigValue = {
+  output: 'export',
+  reactStrictMode: process.env.RUN_MODE === 'develop',
+  experimental: {
+    optimizePackageImports: ['@mantine/core', '@mantine/hooks'],
+  },
+};
+
+const nextConfig =
+  process.env.RUN_MODE === 'local'
+    ? withNextIntlPlugin(withBundleAnalyzer(nextConfigValue))
+    : withNextIntlPlugin(nextConfigValue);
 
 export default nextConfig;
---
+++ src/components/layouts/MantineLogo.tsx
@@ -0,0 +1,15 @@
+import { MantineLogoRounded } from './MantineLogoRounded';
+import { MantineLogoText } from './MantineLogoText';
+import { LogoProps } from './useMantineLogoColors';
+
+export interface MantineLogoProps extends LogoProps {
+  type?: 'mark' | 'full';
+}
+
+export function MantineLogo({ type, ...others }: MantineLogoProps) {
+  if (type === 'mark') {
+    return <MantineLogoRounded {...others} />;
+  }
+
+  return <MantineLogoText {...others} />;
+}
