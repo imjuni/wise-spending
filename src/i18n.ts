26a754114c7a217cd8925bd08473a5f16442b369
ByungJoon Lee <imjuni@gmail.com>
2024-06-23 20:37:33 +0900

    fix(i18n): fix problem redirect
    - fix problem redirect: redirect with assetsPrefix
    - change support languages that using constant variables

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,8 +1,9 @@
+import { languages } from '#/constants/config';
 import { getRequestConfig } from 'next-intl/server';
 import { notFound } from 'next/navigation';
 
 // Can be imported from a shared config
-const locales = ['en', 'de'];
+const locales = languages;
 
 export default getRequestConfig(async ({ locale }) => {
   // Validate that the incoming `locale` parameter is valid
