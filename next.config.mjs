4bfe2c3ed5a470fa1eb9e2f98deeb6404d4d33d3
ByungJoon Lee <imjuni@gmail.com>
2024-06-23 21:19:40 +0900

    feat(route): change Routing Path Logic
    - add settings for assetsPrefix and basePath
    - enhance npm scripts to properly reflect assetsPrefix and basePath settings
    - add .env to handle basePath

--- next.config.mjs
+++ next.config.mjs
@@ -4,11 +4,23 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withBundleAnalyzer = nextBundleAnalyzer({ enabled: process.env.ANALYZE === 'true' });
 const withNextIntlPlugin = createNextIntlPlugin();
 
+const isProd = process.env.NODE_ENV === 'production';
+const basePath = isProd ? '/wise-spending' : '';
+
+console.log('BASE_PATH: ', basePath);
+
 /** @type {import('next').NextConfig} */
 const nextConfigValue = {
+  /**
+   * BASE_PATH는 자동 적용이 되는 것과 그렇지 않은 것이 있다. Link는 자동 적용이고, Image는 직접 적용해야 한다
+   * 그래서 ENV로 넘겨주고, 직접 적용해야 하는 경우 이것을 사용한다
+   *
+   * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
+   */
   output: 'export',
   reactStrictMode: process.env.RUN_MODE === 'develop',
-  assetPrefix: '/wise-spending',
+  assetPrefix: isProd ? 'https://imjuni.github.io/wise-spending' : undefined,
+  basePath: basePath,
   experimental: {
     optimizePackageImports: ['@mantine/core', '@mantine/hooks'],
   },
